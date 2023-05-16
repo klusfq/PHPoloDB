3777aa52aacd4f0178c1c01e0cca0807da36a85a
klusfq <fuquan.yoxi@qq.com>
2023-05-16 12:38:24 +0800

    update: 声明主要接口

---
+++ src/Collection.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace \Fuquan\Pholo;
+
+use Document;
+
+class Collection {
+    /**
+     * 插入一条记录
+     *
+     * @return  bool     插入成功或失败
+     */
+    public function insert(array $row) bool
+    {
+    }
+
+    /**
+     * 插入一条记录
+     * @param   []Docmument     $rows   插入文档列表
+     *
+     * @return  int             影响文档数
+     */
+    public function insertBatch(array $rows) int
+    {
+    }
+
+    /**
+     * 查询数据
+     * @param   array           $fields     元数据
+     * @param   array           $conds      查询条件
+     *
+     * @return  []Document      文档列表
+     */
+    public function find(array $fields, array $conds) array
+    {
+    }
+
+    /**
+     * 更新数据
+     * @param   array           $row        更新文档
+     * @param   array           $conds      查询条件
+     *
+     * @return  int             影响文档数
+     */
+    public function update(array $row, array $conds) int
+    {
+    }
+
+    /**
+     * 删除数据
+     * @param   array           $conds      查询条件
+     *
+     * @return  []Document      文档列表
+     */
+    public function delete(array $conds) array
+}
---
+++ src/Database.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace \Fuquan\Pholo;
+
+use Collection;
+
+class Database
+{
+    /**
+     * 打开文件
+     */
+    public static function openFile(string $filename) Database
+    {
+    }
+
+    /**
+     * 选择collection
+     */
+    public function collection(string $col) Collection
+    {
+    }
+}
