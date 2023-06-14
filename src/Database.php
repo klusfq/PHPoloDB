88dea9024516c28b527d5c7dd5266d254cc79cd3
klusfq <fuquan.yoxi@qq.com>
2023-06-14 17:34:08 +0800

    update

--- src/Database.php
+++ src/Database.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace \Fuquan\Pholo;
+namespace Fuquan\Pholo;
 
 use Collection;
 
@@ -8,13 +8,13 @@ class Database
 {
     const HEADER_FILE = PRO_INIT_PATH . '/phpolodb.h';
 
-    protected static $ffi;
-    protected static $dbCtx;
+    protected static $ffi = NULl;
+    protected static $dbCtx = NULL;
 
     /**
      * 打开文件
      */
-    public static function openFile(string $filename) Database
+    public static function openFile(string $filename): Database
     {
         /**
          * 静态变量
@@ -23,12 +23,14 @@ class Database
          */
         self::$ffi = \FFI::load(self::HEADER_FILE);
         self::$dbCtx = self::$ffi->PLDB_open($filename);
+
+        return new Database();
     }
 
     /**
      * 选择collection
      */
-    public function collection(string $colName) Collection
+    public function collection(string $colName): Collection
     {
         $col = new Collection();
     }
